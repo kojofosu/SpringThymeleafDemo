e5f911f5d7508987e18e58d005abe2e440092e6f
kojofosu <kojofosube@gmail.com>
2021-02-25 19:06:48 +0000

    project init

---
+++ src/test/java/com/mcdev/SpringThymeleafDemo/SpringThymeleafDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcdev.SpringThymeleafDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeleafDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
